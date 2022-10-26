d7dc8867bd9bcfa5b9a361d46508a2a47014cc1a
Cnromana <christine.nakonechnyy@gmail.com>
2022-10-26 12:48:21 -0600

    Submitting again!

--- Exam_2/Exam_2_cn.R
+++ Exam_2/Exam_2_cn.R
@@ -33,20 +33,33 @@ dat %>% group_by(year) %>%
   geom_point() +facet_wrap(~Continent)+scale_x_discrete(breaks = c(1960,1980,2000))+
   theme(legend.position="none")
 
+#geom_line destorys my graph, and will leave it blank, can't figure out why
+#doing points because it is better than not having anything.
+
 class(dat$Under_5_mortality_rate)
 
 #now I will make a line plot of the means of each continent over time
 
 glimpse(dat)
 
-dat %>% group_by(Continent) %>% 
-  mean(dat$Under_5_mortality_rate)
+class(dat$Under_5_mortality_rate)
 
-#can't figure it out SKIPPPPPPPPP for now
 
-#mod1 year
-#mod2 year and continent
-#mod3 year, continent and thier interactionterm
+dat <- dat %>% mutate(Under_5_mortality_rate_n= as.numeric(Under_5_mortality_rate))
+
+dat %>% group_by(Continent) %>%
+  ggplot(mapping = aes(x = year,y = Under_5_mortality_rate,color=Continent)) +
+  geom_point()+geom_smooth() +scale_x_discrete(breaks = c(1960,1980,2000))+
+  theme_minimal()
+  
+
+
+#having lots of trouble, errors keep insisting that the mortality rates are not numeric
+#even when mutated to a numeric.Mean refuses to calculate no matter how I do it, because it is not
+#numeric.
+# :(
+
+#time to make some models!
 
 
 mod1 <- glm(data = dat, 
@@ -62,3 +75,14 @@ compare_performance(mod1,mod2,mod3)
 
 #from what I see here, model 3 has the best r2, with AIC and BIC being lower and RMSE and Sigma
 #also being lower
+
+
+dat %>% gather_predictions(mod1,mod2,mod3,type="response") %>% 
+  ggplot(aes(x = year, y = pred, color=Continent))+geom_point()+
+  geom_smooth(method = "lm",se=FALSE)+
+  facet_wrap(~model)+
+  theme_minimal()+scale_x_discrete(breaks = c(1960,1980,2000))
+
+#again geom_line hates me and will not bring anything up, geom_point is all I got.
+#if you use the same code and replace geom_point with geom_line
+#I guarantee it will show the lines, but my plots go blank when I do it
